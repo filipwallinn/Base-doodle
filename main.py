98d5c7b405da99a4755fa9948fc46282612c0453
filipwallinn <69583965+filipwallinn@users.noreply.github.com>
2025-10-02 12:49:36 +0200

    fkacaption

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from imports import *
 from my_doodle.user_interface import *
-from my_doodle.spotipy_test import *
+from my_doodle.spotipy_logic import *
 
-play_artists_top_song("36QJpDe2go2KgaRleHCDTp")
+# play_artists_top_song("36QJpDe2go2KgaRleHCDTp")
--- my_doodle/user_interface.py
+++ my_doodle/user_interface.py
@@ -1,31 +1,51 @@
 from dearpygui.dearpygui import *
 
-def start_app():
-    print("Starting the app...")
+# def start_app():
+#     print("Starting the app...")
 
-def open_settings():
-    print("Opening settings...")
+# def open_settings():
+#     print("Opening settings...")
 
 def exit_app():
     stop_dearpygui()
+
+# Callback function triggered when the button is clicked
+def handle_input(sender, app_data, user_data):
+    user_text = get_value("artist_input")
+    print(f"User typed: {user_text}")
+    # You can now use `user_text` to search Spotify or do anything else
+
+# Create a simple window with input and button
+# with window(label="Search Artist", width=400, height=200):
+#     add_input_text(label="Enter artist name", tag="artist_input", width=300)
+#     add_button(label="Search and Play", callback=handle_input)
+
+# start_dearpygui()
+
 
 # Create context
 create_context()
 
 with window(label="Bass Doodle", width=600, height=400):
 
-    add_child_window(autosize_x=True, autosize_y=True, border=False)
+#    add_child_window(autosize_x=True, autosize_y=True, border=False)
 
     add_spacer(height=20)
     add_text("Time to Quiz", color=[255, 255, 0])
     add_spacer(height=10)
-    
-    add_button(label="Start", callback=start_app)
-    add_button(label="Settings", callback=open_settings)
-    add_button(label="Exit", callback=exit_app)
+
+    add_input_text(label="Enter artist name", tag="artist_input", width=300)
+    add_button(label="Search and Play", callback=handle_input)
+
+    # add_button(label="Start", callback=start_app)
+    # add_button(label="Settings", callback=open_settings)
 
     add_spacer(height=20)
     add_text("Made for my Jules <3", wrap=0)
+
+    add_spacer(height=50)
+    add_button(label="Exit", callback=exit_app)
+
 
 # Setup and show viewport
 create_viewport(title='Bass Doodle', width=600, height=400)
